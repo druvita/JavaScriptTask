d41b1901e7aabfe87c50714d5302ef6162edb6b5
dhruvita <dhruvita02.exhibyte@gmail.com>
2021-07-01 16:33:01 +0530

    commit

---
+++ script.js
@@ -0,0 +1,123 @@
+/*let js = 'amazing';
+    //console.log(40+8+23-10);
+
+    console.log('jonas');
+    console.log(23);
+
+    let firstName = "Matila";
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+    
+    //variable name convention
+    let jonas_matilda = "JM";
+    let $function = 27;
+
+    let person = "jonas";
+    let PI = 3.1415;
+
+    let myFirstJob = "Coder";
+    let myCurrentJob = "Teacher";
+
+    let job1 = "programmer";
+    let job2 = "teacher";
+
+    console.log(myFirstJob);
+
+ let javascriptIsFun = true;
+ console.log(javascriptIsFun);
+
+ //console.log(typeof true);
+ console.log(typeof javascriptIsFun);
+ //console.log(typeof 23);
+ //console.log(typeof 'Jonas');
+
+ javascriptIsFun = 'Yes';
+ console.log(typeof javascriptIsFun);
+
+ let year;
+ console.log(year);
+ console.log(typeof year);
+
+ year = 1991;
+ console.log(typeof year);
+
+ console.log(typeof null)
+
+ 
+
+
+let age = 30;
+age = 31;
+
+
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Goyani';
+console.log(lastName);
+const now = 2037;
+const agedhruvi = now - 1991;
+const agevimal = now - 2018;
+console.log(agedhruvi, agevimal);
+
+console.log(agedhruvi * 2, agevimal / 10, 2** 3)
+//2***3 means 2 to the power of 3= 2*2*2
+
+const firstName = 'dhruvi';
+const lastName = 'Goyani';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; //x=x+10
+x *=4; //x =x*4 =100
+x++;// x = x+ 1
+x--;
+x--;
+console.log(x);
+
+//Comarison Operator
+console.log(agedhruvi > agevimal);
+console.log(agevimal >= 18);
+
+const isFullAge = agedhruvi >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+const now = 2037;
+const agedhruvi = now - 1991;
+const agevimal = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y= 25 - 10-5;
+console.log(x,y);
+
+const averageAge = (agedhruvi + agevimal) /2
+console.log(agedhruvi, agevimal, averageAge);
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+*/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm" + firstName +', a' + (year - birthYear) +' years old ' + job + '!';
+console.log(jonas);
